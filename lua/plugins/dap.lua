389a23169f3d95abb3f829820bc53880be6e379d
羽原圭吾 <habarakeigo@habarakeigonoMacBook-Air.local>
2021-08-20 06:50:54 +0900

    Update DAP configuration

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -5,89 +5,63 @@ end
 
 require('dap').set_log_level('TRACE')
 
--- Python
+-- Python: https://github.com/mfussenegger/nvim-dap-python#usage
 require('dap-python').setup('~/.asdf/shims/python3')
 
--- C++
-dap.adapters.cpp = {
+
+-- C/C++, Rust: https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#ccrust-via-lldb-vscode
+dap.adapters.lldb = {
     type = 'executable',
-    attach = {
-        pidProperty = "pid",
-        pidSelect = "ask"
-    },
     command = 'lldb-vscode',
-    env = {
-        LLDB_LAUNCH_FLAG_LAUNCH_IN_TTY = "YES"
-    },
     name = "lldb"
 }
 
 dap.configurations.cpp = {
     {
-        name = "lldb",
-        type = "cpp",
+        name = "Launch",
+        type = "lldb",
         request = "launch",
         program = function()
             return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
         end,
         cwd = '${workspaceFolder}',
-        externalTerminal = false,
         stopOnEntry = false,
-        args = {}
+        args = {},
+        runInTerminal = false,
     },
 }
 
--- Rust
-dap.adapters.rust = dap.adapters.cpp
-
+dap.configurations.c = dap.configurations.cpp
 dap.configurations.rust = {
     {
-        type = "rust",
-        name = "Debug",
+        name = "Launch",
+        type = "lldb",
         request = "launch",
         program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
         end,
-    }
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = {},
+        runInTerminal = false,
+    },
 }
 
--- Go
-dap.adapters.go = function(callback, config)
-    local handle
-    local pid_or_err
-    local port = 38697
-    handle, pid_or_err =
-    vim.loop.spawn(
-    "dlv",
-    {
-        args = {"dap", "-l", "127.0.0.1:" .. port},
-        detached = true
-    },
-    function(code)
-        handle:close()
-        print("Delve exited with exit code: " .. code)
-    end
-    )
-    ----should we wait for delve to start???
-    --vim.defer_fn(
-    --function()
-    --dap.repl.open()
-    --callback({type = "server", host = "127.0.0.1", port = port})
-    --end,
-    --100)
-    dap.repl.open()
-    callback({type = "server", host = "127.0.0.1", port = port})
-end
--- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+
+-- Go: https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#Go
+dap.adapters.go = {
+    type = "server",
+    host = "127.0.0.1",
+    port = 38697,
+}
 dap.configurations.go = {
     {
-        type = "go",
-        name = "Debug",
-        request = "launch",
-        program = "${file}"
+      type = "go",
+      name = "Debug",
+      request = "launch",
+      program = "${file}"
     }
 }
-
--- start delve in DAP mode manually from your project folder by running the following command before start debugging.
--- dlv dap -l 127.0.0.1:38697 --log --log-output="dap"
+-- Execute in the same directory
+-- $ dlv dap -l 127.0.0.1:38697 --log --log-output="dap"
 
